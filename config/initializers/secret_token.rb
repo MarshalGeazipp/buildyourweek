9127d11476c1a2f10b8599c28f0e9aa2236719c3
Sebastian Obermüller <Sebastian.Obermueller@burda.com>
2011-12-09 15:27:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buildyourweek::Application.config.secret_token = '0e9bb6b952b05fd4989bf3504992f25b0bda4bb4c1d32af6716daa923c92c2f17a3fa6783e1087d528e51392a18a3ca6ce77878b28c1332c3985551df7a1313e'
